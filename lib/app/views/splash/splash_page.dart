d69420b34e44b75146af8a21796387da37d74116
alifzr <alifzakya.r@gmail.com>
2025-03-02 00:51:48 +0700

    implement biometric fingerprint with local_auth if user has logged in

--- lib/app/views/splash/splash_page.dart
+++ lib/app/views/splash/splash_page.dart
@@ -1,13 +1,18 @@
 import 'dart:async';
 
+import 'package:epenting/app/cubits/auth/auth_cubit.dart';
 import 'package:epenting/app/utils/app_strings.dart';
+import 'package:epenting/app/views/dashboard/dashboard_page.dart';
 import 'package:epenting/app/views/login/login_page.dart';
 import 'package:epenting/app/views/onboard/onboard_page.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:local_auth/local_auth.dart';
+import 'package:local_auth_android/local_auth_android.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -21,6 +26,7 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
+  final localAuthentication = LocalAuthentication();
   Timer? _timer;
   String? _appVersion;
 
@@ -36,16 +42,88 @@ class _SplashPageState extends State<SplashPage> {
     }
   }
 
+  Future<bool> _checkBiometrics() async {
+    try {
+      final canAuthenticateWithBiometrics =
+          await localAuthentication.canCheckBiometrics;
+      final canAuthenticate =
+          canAuthenticateWithBiometrics ||
+          await localAuthentication.isDeviceSupported();
+
+      return canAuthenticate;
+    } on PlatformException catch (e) {
+      if (kDebugMode) print(e.message);
+      return false;
+    }
+  }
+
+  Future<bool> _authFingerprint() async {
+    try {
+      final didAuthFingerprint = await localAuthentication.authenticate(
+        localizedReason: 'Silahkan pindai sidik jari anda untuk melanjutkan',
+        authMessages: [
+          AndroidAuthMessages(
+            signInTitle: 'E-Penting',
+            biometricHint: 'Masuk dengan sidik jari',
+            cancelButton: 'Batal',
+            biometricNotRecognized: 'Sidik jari tidak dikenali, coba lagi',
+            biometricSuccess: 'Autentikasi berhasil',
+          ),
+        ],
+        options: AuthenticationOptions(
+          biometricOnly: true,
+          stickyAuth: true,
+          sensitiveTransaction: true,
+          useErrorDialogs: true,
+        ),
+      );
+
+      return didAuthFingerprint;
+    } on PlatformException catch (e) {
+      if (kDebugMode) print(e.message);
+      return false;
+    }
+  }
+
   void _initTimer() async {
+    bool hasToken = await context.read<AuthCubit>().hasToken();
     final sharedPreferences = await SharedPreferences.getInstance();
     bool openedOnboard = sharedPreferences.getBool('opened_onboard') ?? false;
 
-    _timer = Timer(const Duration(seconds: 2), () {
-      Navigator.pushNamedAndRemoveUntil(
-        context,
-        openedOnboard ? LoginPage.routeName : OnboardPage.routeName,
-        (route) => false,
-      );
+    _timer = Timer(const Duration(seconds: 2), () async {
+      if (hasToken) {
+        final canAuthenticate = await _checkBiometrics();
+
+        if (canAuthenticate) {
+          final didAuthFingerprint = await _authFingerprint();
+
+          if (didAuthFingerprint) {
+            if (mounted) {
+              Navigator.pushNamedAndRemoveUntil(
+                context,
+                DashboardPage.routeName,
+                (route) => false,
+              );
+            }
+          } else {
+            SystemNavigator.pop();
+          }
+        } else {
+          if (mounted) {
+            Navigator.pushNamedAndRemoveUntil(
+              context,
+              DashboardPage.routeName,
+              (route) => false,
+            );
+          }
+        }
+      } else {
+        Navigator.pushNamedAndRemoveUntil(
+          context,
+          openedOnboard ? LoginPage.routeName : OnboardPage.routeName,
+          (route) => false,
+        );
+      }
     });
   }
 
