c8f70fdf7330e2266fc1cd7fec99547d1f290a92
alifzr <alifzakya.r@gmail.com>
2025-03-03 00:19:21 +0700

    add percentage endpoint

--- lib/app/utils/end_point.dart
+++ lib/app/utils/end_point.dart
@@ -5,4 +5,6 @@ class EndPoint {
   static const String classification = '/classification';
   static const String weight = '/berat-badan';
   static const String nutrition = '/gizi';
+
+  static const String percentage = '/percentage';
 }
