d921eef772c2fd93e5b793b916c3f097375ac2ff
alifzr <alifzakya.r@gmail.com>
2025-03-03 00:17:45 +0700

    .

--- lib/app/configs/router/page_transition.dart
+++ lib/app/configs/router/page_transition.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
 
-PageTransition<dynamic> fadeTransition(
+PageTransition<dynamic> pageTransition(
   BuildContext context,
   RouteSettings settings, {
+  PageTransitionType type = PageTransitionType.fade,
   required Widget child,
 }) {
   return PageTransition(
-    type: PageTransitionType.fade,
+    type: type,
     childCurrent: context.currentRoute,
     settings: settings,
     curve: Curves.easeInOutCubic,
