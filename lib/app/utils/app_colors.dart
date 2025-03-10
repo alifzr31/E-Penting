e83161e02b102d3b464d21bdb630bd4c8ba86480
alifzr <alifzakya.r@gmail.com>
2025-03-10 16:00:03 +0700

    add some color

--- lib/app/utils/app_colors.dart
+++ lib/app/utils/app_colors.dart
@@ -6,4 +6,10 @@ class AppColors {
   static const Color blueColor = Color(0xFF3653DB);
   static const Color softBlueColor = Color(0xFFDCE1F7);
   static const Color greyFormField = Color(0xFFB5B8BA);
+  static const Color maleColor = Color(0xFF81B5FE);
+  static const Color femaleColor = Color(0xFFFE7366);
+  static const Color normalColor = Color(0xFF45C3A4);
+  static const Color veryShortColor = Color(0xFFFE7366);
+  static const Color shortColor = Color(0xFFFD8F52);
+  static const Color tallColor = Color(0xFF3482FE);
 }
