01bdbff490b3e98ebe67b4e0d350b849e47070b5
alifzr <alifzakya.r@gmail.com>
2025-04-08 13:50:43 +0700

    .

--- lib/app/utils/app_helpers.dart
+++ lib/app/utils/app_helpers.dart
@@ -1,5 +1,6 @@
 import 'package:epenting/app/utils/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart';
 import 'package:intl/intl.dart';
 
 class AppHelpers {
@@ -11,6 +12,41 @@ class AppHelpers {
         : stanting?.toLowerCase() == 'tinggi'
         ? AppColors.tallColor
         : AppColors.normalColor;
+  }
+
+  static IconData genderIcon(String gender) {
+    return gender.toLowerCase() == 'laki-laki'
+        ? MingCute.male_line
+        : MingCute.female_line;
+  }
+
+  static Color genderColor(String gender) {
+    return gender.toLowerCase() == 'laki-laki'
+        ? AppColors.maleColor
+        : AppColors.femaleColor;
+  }
+
+  static String ageYearAndMonth(DateTime birthDate) {
+    DateTime now = DateTime.now();
+
+    int year = now.year - birthDate.year;
+    int month = now.month - birthDate.month;
+
+    if (month < 0) {
+      year--;
+      month += 12;
+    }
+
+    return "$year Tahun $month Bulan";
+  }
+
+  static String panduanImage(String ageRange) {
+    final imageName = ageRange.toLowerCase().replaceAll(' ', '');
+    return '$imageName.jpg';
+  }
+
+  static String dayMonthYearFormat(DateTime date) {
+    return DateFormat('dd MMMM yyyy').format(date);
   }
 
   static String monthYearFormat(DateTime date) {
