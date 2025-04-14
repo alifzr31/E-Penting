6303587d8f223a488895081bd43c7234e73a6232
alifzr <alifzakya.r@gmail.com>
2025-04-14 15:00:19 +0700

    .

--- lib/app/utils/app_helpers.dart
+++ lib/app/utils/app_helpers.dart
@@ -52,4 +52,16 @@ class AppHelpers {
   static String monthYearFormat(DateTime date) {
     return DateFormat('MMMM yyyy').format(date);
   }
+
+  static String databaseDateFormat(DateTime date) {
+    return DateFormat('yyyy-MM-dd').format(date);
+  }
+
+  static String formatDoubleToString(double value) {
+    if (value == value.toInt()) {
+      return value.toInt().toString();
+    } else {
+      return value.toString();
+    }
+  }
 }
