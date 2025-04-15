2c9658dbebfd2221ef0c6874ec0abcb069a641ce
alifzr <alifzakya.r@gmail.com>
2025-04-15 14:43:51 +0700

    upgrade dependencies version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -75,7 +75,7 @@ dependencies {
     implementation("androidx.window:window-java:1.3.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.multidex:multidex:2.0.1")
-    implementation(platform("com.google.firebase:firebase-bom:33.10.0"))
+    implementation(platform("com.google.firebase:firebase-bom:33.12.0"))
     implementation("com.google.firebase:firebase-analytics")
-    implementation("com.google.firebase:firebase-messaging:24.1.0")
+    implementation("com.google.firebase:firebase-messaging:24.1.1")
 }
